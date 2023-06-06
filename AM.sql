3ea4e8b95c41eac241c4a958fe53947db69322e3
attarmau <87364730+attarmau@users.noreply.github.com>
2023-06-06 10:39:37 +0800

    Update AM.sql

--- AM.sql
+++ AM.sql
@@ -12,14 +12,21 @@ CREATE TABLE your_table_name (
   -- other column definitions
 );
 
-# increase order; decrease order
+# Increase order; decrease order
 CREATE VIEW your_view_name AS
 SELECT email_address, first_name, last_name, rank
 FROM your_table_name
 ORDER BY rank ASC, last_name DESC, first_name DESC;
 
-# behave more like a keyword search for phrases
+# Behave more like a keyword search for phrases
 ## The LIKE operator is used in the WHERE clause to perform pattern matching. The percent sign (%) is a wildcard that matches any sequence of characters.
 SELECT * FROM data_table WHERE search_term LIKE '%<user_input>%';
 
-# Write a SQL query to get the average review ratings for every product every month.
+# Write a SQL query to get the average review ratings for every product every month
+SELECT
+  EXTRACT(MONTH FROM submit_date) as mth,
+  product_id,
+  ROUND(AVG(stars),2) AS avg_stars
+FROM reviews
+GROUP BY EXTRACT(MONTH FROM submit_date), product_id
+ORDER BY mth, product_id

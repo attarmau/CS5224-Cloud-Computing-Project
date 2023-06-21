1c600751a7c30182ff95c618bd8fbd0d6d8daf85
attarmau <87364730+attarmau@users.noreply.github.com>
2023-06-22 01:47:16 +0800

    Update iOS.networkingapp.swift

--- iOS.networkingapp.swift
+++ iOS.networkingapp.swift
@@ -1,17 +1,1 @@
-func factorial(of number: Int) -> Int {
-    if number == 0 {
-        return 1
-    }
-    
-    var result = 1
-    for i in 1...number {
-        result *= i
-    }
-    
-    return result
-}
 
-// Test the factorial function
-let number = 5
-let result = factorial(of: number)
-print("The factorial of \(number) is \(result)")

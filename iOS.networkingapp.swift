5971a5cf51527026682429c607bb7f87741fdc5d
attarmau <87364730+attarmau@users.noreply.github.com>
2023-07-28 00:49:43 +0800

    Update iOS.networkingapp.swift

--- iOS.networkingapp.swift
+++ iOS.networkingapp.swift
@@ -183,3 +183,28 @@ func playSound() {
     } catch {
         print("Error playing sound: \(error)")
     }
+    
+        // Function to capture an image using the device's camera
+    func captureImageFromCamera() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    // Function to handle the captured image and display it in an image view
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            let imageView = UIImageView(frame: CGRect(x: 50, y: 250, width: 200, height: 200))
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            view.addSubview(imageView)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    // Function to handle image picker cancellation
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
